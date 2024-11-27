49da3cab51f69a10f142082604e33b4b6457a286
Nahid <nahidafshan001@gmail.com>
2024-11-27 21:52:41 +0800

    My first Spring project

---
+++ MySpringProj/src/test/java/org/example/myspringproj/MySpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.myspringproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
