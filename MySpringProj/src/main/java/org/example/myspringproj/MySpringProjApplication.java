40ccf705a7e86c2586ba6249eb272ce7e4d2ceef
Nahid <nahidafshan001@gmail.com>
2024-11-27 22:00:43 +0800

    Added comment

--- MySpringProj/src/main/java/org/example/myspringproj/MySpringProjApplication.java
+++ MySpringProj/src/main/java/org/example/myspringproj/MySpringProjApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//testing
 @SpringBootApplication
 @RestController
 public class MySpringProjApplication {
